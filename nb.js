325e5eed33569263b6894b8f7e691e722daf0b62
Ryan Bishop <rdbishop06@gmail.com>
2021-02-14 15:12:08 -0600

    Remove unused variable

--- nb.js
+++ nb.js
@@ -56,7 +56,7 @@ function setProbabilityOfChordsInLabels() {
 	probabilityOfChordsInLabels = chordCountsInLabels;
 	Object.keys(probabilityOfChordsInLabels).forEach(function(difficulty) {
 		Object.keys(probabilityOfChordsInLabels[difficulty]).forEach(function(chord) {
-			probabilityOfChordsInLabels[difficulty][chord] = probabilityOfChordsInLabels[difficulty][chord] * 1.0 / songs.length;
+			probabilityOfChordsInLabels[difficulty][chord] = probabilityOfChordsInLabels[difficulty][chord] / songs.length;
 		});
 	});
 }
@@ -73,10 +73,9 @@ setLabelProbabilities();
 setChordCountsInLabels();
 setProbabilityOfChordsInLabels();
 function classify(chords) {
-	var total = labelProbabilities;
-	console.log(total);
+	console.log(labelProbabilities);
 	var classified = {};
-	Object.keys(total).forEach(function(difficulty) {
+	Object.keys(labelProbabilities).forEach(function(difficulty) {
 		var first = labelProbabilities[difficulty] + 1.01;
 		chords.forEach(function(chord) {
 			var probabilityOfChordInLabel = probabilityOfChordsInLabels[difficulty][chord];
